a34ac75b1d86569203eeb0cd894f725a8df07ebb
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-18 15:30:42 +0700

    cleanup: remove obsolete bridge files
    - Remove buf/validate/bridge.go and source/v1/bridge.go
    - These files are no longer needed after protobuf refactoring

--- buf/validate/bridge.go
+++
@@ -1,7 +0,0 @@
-// Package validate provides a bridge to the actual generated protobuf validation types
-package validate
-
-// Re-export all types from the actual generated location
-import (
-	_ "github.com/tigorlazuardi/claw/lib/claw/gen/proto/buf/validate"
-)
