e897b2054fbffb863fecf68046fd8944fa78477e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-08-30 01:24:29 +0700

    feat(scheduler): added simpler job tracking mechanism

--- lib/claw/scheduler.go
+++ lib/claw/scheduler.go
@@ -3,12 +3,52 @@ package claw
 import (
 	"context"
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"time"
 
+	"github.com/tigorlazuardi/claw/lib/claw/config"
 	"github.com/tigorlazuardi/claw/lib/claw/gen/jet/model"
 	clawv1 "github.com/tigorlazuardi/claw/lib/claw/gen/proto/claw/v1"
 	"github.com/tigorlazuardi/claw/lib/claw/types"
 )
+
+type scheduler struct {
+	claw        *Claw
+	config      *config.Config
+	isRunning   atomic.Bool
+	tracker     *tracker
+	queue       chan model.Jobs
+	baseContext func() context.Context
+}
+
+type tracker struct {
+	sync.Map
+}
+
+func (qt *tracker) Add(jobID int64) {
+	qt.Store(jobID, struct{}{})
+}
+
+func (qt *tracker) Remove(jobID int64) {
+	qt.Delete(jobID)
+}
+
+func (qt *tracker) Exists(jobID int64) bool {
+	_, exists := qt.Load(jobID)
+	return exists
+}
+
+func (qt *tracker) List() []int64 {
+	var jobs []int64
+	qt.Range(func(key, value any) bool {
+		if id, ok := key.(int64); ok {
+			jobs = append(jobs, id)
+		}
+		return true
+	})
+	return jobs
+}
 
 // StartScheduler begins the scheduler event loop
 func (c *Claw) StartScheduler(ctx context.Context) {
